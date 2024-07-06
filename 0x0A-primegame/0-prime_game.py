6a58b4611c010caa77f2fedde9236da074f8fb1c
Amal-Dahshan <amaldahshan11710@gmail.com>
2024-07-06 12:06:06 -0700

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,47 +1,48 @@
 #!/usr/bin/python3
-""" Prime game """
+"""
+Prime game DSA
+"""
 
 
-def is_prime(num):
-    """Checks if a number is prime."""
-    if num <= 1:
+def isPrime(num):
+    """Function to determine if a number is prime or not"""
+    if num == 1:
         return False
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, num):
         if num % i == 0:
             return False
     return True
 
 
-def remove_multiples(nums, prime):
-    """Removes the prime and its multiples from the list."""
-    new_nums = []
-    for num in nums:
-        if num % prime != 0:
-            new_nums.append(num)
-    return new_nums
-
-
 def isWinner(x, nums):
-    """Determines the winner of a game."""
-    maria_wins = 0
-    ben_wins = 0
-    for _ in range(x):
-        player = "Maria"
-        while nums:
-            if player == "Maria":
-                # Find first prime in the list
-                for num in nums:
-                    if is_prime(num):
-                        nums = remove_multiples(nums, num)
-                        player = "Ben"
-                        break
-            else:
-                # Ben has no choice but to pick the first number
-                nums = remove_multiples(nums, nums[0])
-                player = "Maria"
-        if nums:
-            ben_wins += 1
+    """Function to determine the winner of a game"""
+    if x > len(nums):
+        return None
+    Maria = 0
+    Ben = 0
+    for round in range(x):
+        turn = 1
+        options = [n + 1 for n in range(nums[round])]
+        i = 1
+        while i < len(options):
+            if isPrime(options[i]):
+                div = options[i]
+                options.pop(i)
+                j = 0
+                while j < len(options):
+                    if options[j] % div == 0:
+                        options.pop(j)
+                    j += 1
+                turn += 1
+                i = 0
+            i += 1
+        if turn % 2 == 0:
+            Maria += 1
         else:
-            maria_wins += 1
-        nums = list(range(1, max(nums) + 1))  # Reset for next round
-    return "Maria" if maria_wins > ben_wins else ("Ben" if ben_wins > maria_wins else None)
+            Ben += 1
+    if Ben > Maria:
+        return 'Ben'
+    elif Maria > Ben:
+        return 'Maria'
+    else:
+        return None
